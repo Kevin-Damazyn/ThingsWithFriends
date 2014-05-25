a87e6e620100fe451c2e3a0eb07f783d0f86a15a
mtabor150 <mtabor150@gmail.com>
2014-05-24 21:51:06 -0500

    wtf mate

--- TWF_Android/src/thingswithfriends/main/MainActivity.java
+++ TWF_Android/src/thingswithfriends/main/MainActivity.java
@@ -1,54 +1,45 @@
 package thingswithfriends.main;
 
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	ListView listView;
-	Button gameButton;
+        findViewById(R.id.start_player).setOnClickListener(
+                new OnClickListener() {
+            public void onClick(View v) {
+                startGame(true);
+            }
+        });
 
-	public void OnCreate(Bundle SavedInstanceState) {
-		super.onCreate(SavedInstanceState);
-		setContentView(R.layout.activity_main);
-		this.gameButton = (Button)this.findViewById(R.id.button_newgame);
+        findViewById(R.id.start_comp).setOnClickListener(
+                new OnClickListener() {
+            public void onClick(View v) {
+                startGame(false);
+            }
+        });
+    }
 
-		// get listview object
-		listView = (ListView) findViewById(R.id.gameList);
-
-		// listview values
-		// TODO get values of games from server.
-		String[] values = new String[] { "Game1", "Game2", "Game3" };
-
-		// Adapter for list
-		ArrayAdapter<String> gameListAdapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
-		// assign adapter to listview
-		listView.setAdapter(gameListAdapter);
-
-		// ListView item click listener (Choosing a game)
-		listView.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				// ListView clicked item index
-				int itemPosition = position;
-
-				// Listview clicked item value
-				String itemValue = (String) listView
-						.getItemAtPosition(itemPosition);
-
-			}
-
-		});
-
-	}
+    private void startGame(boolean startWithHuman) {
+        /*Intent i = new Intent(this, GameActivity.class);
+        i.putExtra(GameActivity.EXTRA_START_PLAYER,
+                startWithHuman ? State.PLAYER1.getValue() : State.PLAYER2.getValue());
+        startActivity(i);*/
+    }
 }
